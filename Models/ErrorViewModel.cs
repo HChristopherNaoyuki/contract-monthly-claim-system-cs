d2aec90d4728c6349beeb42fa6f90a17e6f87340
HChristopherNaoyuki <st10462415@rcconnect.edu.za>
2025-08-18 14:33:58 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace contract_monthly_claim_system_cs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
